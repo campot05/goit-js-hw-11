a88bbe4234e498ac5f6d51402f9b4c1457348c8c
campot05 <daniil.spectr@gmail.com>
2023-01-28 20:38:02 +0200

    add function create markup

---
+++ src/index.js
@@ -0,0 +1,85 @@
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+import axios from 'axios';
+
+const API_KEY = '33190219-0860edc2b5cf578f738ea4f26';
+
+const refs = {
+  input: document.querySelector('.input'),
+  form: document.querySelector('.search-form'),
+  gallery: document.querySelector('.gallery'),
+};
+
+refs.form.addEventListener('submit', e => {
+  e.preventDefault();
+  const value = refs.input.value;
+
+  try {
+    fetchImg(API_KEY, value).then(res => {
+      refs.gallery.insertAdjacentHTML('beforeend', createItemGallety(res));
+    });
+  } catch {
+    Notify.failure(
+      'Sorry, there are no images matching your search query. Please try again.'
+    );
+  }
+});
+
+function fetchImg(key, value) {
+  return axios
+    .get('https://pixabay.com/api/', {
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      params: {
+        key: `${API_KEY}`,
+        q: `${value}`,
+        image_type: 'photo',
+        orientation: 'horizontal',
+        safesearch: true,
+      },
+    })
+    .then(response => response.data.hits)
+    .catch(error => console.log(error));
+}
+
+function createItemGallety(items) {
+  return items
+    .map(
+      ({
+        webformatURL,
+        largeImageURL,
+        tags,
+        likes,
+        views,
+        comments,
+        downloads,
+      }) => {
+        return `
+	<div class="photo-card">
+	<a class="photo-card__link" href="${largeImageURL}">
+		<img src="${webformatURL}" alt="${tags}" loading="lazy" />
+	</a>
+	<div class="info">
+	  <p class="info-item">
+		 <b>Likes</b>
+		 ${likes}
+	  </p>
+	  <p class="info-item">
+		 <b>Views</b>
+		 ${views}
+	  </p>
+	  <p class="info-item">
+		 <b>Comments</b>
+		 ${comments}
+	  </p>
+	  <p class="info-item">
+		 <b>Downloads</b>
+		 ${downloads}
+	  </p>
+	</div>
+ </div>
+	`;
+      }
+    )
+    .join('');
+}
