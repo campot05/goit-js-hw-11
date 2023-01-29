d15cd1476cd907e4d4be9e65b05db403c6d06454
campot05 <daniil.spectr@gmail.com>
2023-01-29 15:33:33 +0200

    add hbs npm and refactoring markup

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import { Notify } from 'notiflix/build/notiflix-notify-aio';
 import axios from 'axios';
 import SimpleLightbox from 'simplelightbox';
 import 'simplelightbox/dist/simple-lightbox.min.css';
+import test from './test.hbs';
 
 const API_KEY = '33190219-0860edc2b5cf578f738ea4f26';
 
@@ -53,45 +54,7 @@ function fetchImg(key, value) {
 }
 
 function createItemGallety(items) {
-  return items
-    .map(
-      ({
-        webformatURL,
-        largeImageURL,
-        tags,
-        likes,
-        views,
-        comments,
-        downloads,
-      }) => {
-        return `
-	<div class="photo-card">
-	<a class="photo-card__link" href="${largeImageURL}">
-		<img src="${webformatURL}" alt="${tags}" loading="lazy" />
-	</a>
-	<div class="info">
-	  <p class="info-item">
-		 <b>Likes</b>
-		 ${likes}
-	  </p>
-	  <p class="info-item">
-		 <b>Views</b>
-		 ${views}
-	  </p>
-	  <p class="info-item">
-		 <b>Comments</b>
-		 ${comments}
-	  </p>
-	  <p class="info-item">
-		 <b>Downloads</b>
-		 ${downloads}
-	  </p>
-	</div>
- </div>
-	`;
-      }
-    )
-    .join('');
+  return items.map(test).join('');
 }
 
 const lightbox = new SimpleLightbox('.gallery a');
